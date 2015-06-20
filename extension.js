afbd0e0937fad58087b366d8622eced2e77c23a2
LadyHell <evellynkarine159@gmail.com>
2015-06-20 20:27:31 -0300

    sdfgfhbfsh
    hdfxhdfh

--- extension.js
+++ extension.js
@@ -17,24 +17,23 @@
 
         /*
         bot.commands.cookieCommand.cookies =['deu-lhe um biscoito de chocolate!',
-                    'deu-lhe um biscoito de aveia caseiro macio!',
+                    'deu-lhe um biscoito de onda forte pra você ver macacos em cima do poste.',
                     'deu-lhe um biscoito podre e sujo. Era o último do pacote. Que nojo!',
-                    'deu-lhe um bolinho de açúcar... O quê? Sem estrelinhas e povilho? Eu não tocaria.',
+                    'deu-lhe um pau pra você enfiar no seu cu.',
                     'deu-lhe um biscoito de chocolate. Oh, não, são passas. Eca!',
-                    'deu-lhe um enorme biscoito. Quando o toca, ele se duplica num outro biscoito... estranho',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Por que você não está trabalhando?"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Cumprimente agora a pessoa que você ama"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Arrisque-se!"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Saia desse computador!"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Não esqueça de comer os vegetais"',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão te achar sexy!',
-                    'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo"',
-                    'deu-lhe um biscoito de ouro, mas não dá pra comer... Droga!',
+                    'deu-lhe um enorme biscoito. Quando o toca, ele se duplica num outro biscoito... queima gzuis!',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Manda nudes"',
+                    'deu-lhe um biscoito de "pó" pra você cheirar, UHEAUEHAUHEAUHE.
+                    'deu-lhe um biscoito da sorte, tem escrito: "Venda sua alma."',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Suprise modafocaa."',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Vai se prostituir pra comprar x-burguer que é melhor pra você."',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão achar que você é funkeiro. UAEHUAHEAUHE',
+                    'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo <3"',
+                    'deu-lhe um biscoito de ouro, mas não dá pra comer porque é ouro.',
                     'deu-lhe um Oreo e um copo de leite.',
-                    'deu-lhe um biscoito de arco-íris feito com amor :heart:',
-                    'deu-lhe um biscoito que foi esquecido na chuva... eu não comeria.',
-                    'te trouxe biscoitos fresquinhos... parecem deliciosos!'
-                ];
+                    'deu-lhe um biscoito de arco-íris feito com amor :heart: ~ai que coisa gay.',
+                    'deu-lhe um biscoito que foi esquecido na chuva. Se eu você você, eu não comeria esse biscoito. :eyes:',
+                    'te trouxe biscoitos fresquinhos... parecem ser deliciosos!!'
         */
         
         bot.commands.testCommand = {
@@ -58,7 +57,7 @@
     //Mudar as configurações padrões do bot e ter certeza que elas foram carregadas corretamente
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
+        botName: "MidnightBOT",
         language: "portuguese",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/pt.json",
         startupCap: 1, // 1-200
@@ -87,13 +86,13 @@
         usercommandsEnabled: true,
         skipPosition: 3,
         skipReasons: [
-        	["theme", "A música não se encaixa nos padrões da sala. "],
+        	["theme", "Essa música não se encaixa nos padrões da sala. "],
         	["op", "Essa música está na lista OP. "],
-        	["history", "A música ainda está no histórico. "],
-        	["mix", "Você tocou um mix (muito longo) - não permitido. "],
-        	["sound", "A música que você tocou tinha má qualidade ou estava sem som. "],
-        	["nsfw", "A música que você tocou é NSFW (impróprio). "],
-        	["unavailable", "A música que você tocou está indisponível. "]
+        	["history", "Essa música ainda tá no histórico, presta atenção, caralho. "],
+        	["mix", "Essa poarr é longa demais, vou pular. "],
+        	["sound", "Essa música não está tocando e o áudio tá muito ruim, vê se não faz de novo na próxima vez. "],
+        	["nsfw", "Essa música que você tocou é NSFW (imprópria), enfia seus NSFW no seu cu. "],
+        	["unavailable", "Essa música não está disponível, chora mais. :joy: "]
         ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
