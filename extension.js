5722756a493c12298e8842062765e3a87e771df1
LadyHell <evellynkarine159@gmail.com>
2015-06-20 20:56:37 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -18,9 +18,9 @@
         /*
         bot.commands.cookieCommand.cookies =['deu-lhe um biscoito de chocolate!',
                     'deu-lhe um biscoito de onda forte pra você ver macacos em cima do poste.',
-                    'deu-lhe um biscoito podre e sujo. Era o último do pacote. Que nojo!',
+                    'deu-lhe um biscoito da sorte. Pera, não tem nada escrito! :joy:',
                     'deu-lhe um pau pra você enfiar no seu cu.',
-                    'deu-lhe um biscoito de chocolate. Oh, não, são passas. Eca!',
+                    'deu-lhe um biscoito de chocolate abençoado por Satã.',
                     'deu-lhe um enorme biscoito. Quando o toca, ele se duplica num outro biscoito... queima gzuis!',
                     'deu-lhe um biscoito da sorte, tem escrito: "Manda nudes"',
                     'deu-lhe um biscoito de "pó" pra você cheirar, UHEAUEHAUHEAUHE.
@@ -29,8 +29,8 @@
                     'deu-lhe um biscoito da sorte, tem escrito: "Vai se prostituir pra comprar x-burguer que é melhor pra você."',
                     'deu-lhe um biscoito da sorte, tem escrito: "Se você mecher o quadril, vão achar que você é funkeiro. UAEHUAHEAUHE',
                     'deu-lhe um biscoito da sorte, tem escrito: "Eu te amo <3"',
-                    'deu-lhe um biscoito de ouro, mas não dá pra comer porque é ouro.',
-                    'deu-lhe um Oreo e um copo de leite.',
+                    'deu-lhe um biscoito de ouro, mas não dá pra comer porque é ouro, burraum.',
+                    'deu-lhe um dicionário, uma caneta e um livro pra você aprender a escrever direito, filho da puta.',
                     'deu-lhe um biscoito de arco-íris feito com amor :heart: ~ai que coisa gay.',
                     'deu-lhe um biscoito que foi esquecido na chuva. Se eu você você, eu não comeria esse biscoito. :eyes:',
                     'te trouxe biscoitos fresquinhos... parecem ser deliciosos!!'
